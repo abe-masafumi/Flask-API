c7fe1192628edf099047b1a850ad640eb72022d7
@Masa455 <83285963+abe-masafumi@users.noreply.github.com>
2023-01-30 16:30:00 +0000

    add:パラメータで指定された情報を返すようにした

--- application.py
+++ application.py
@@ -1,34 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 
 app = Flask(__name__)
 
-# --- Costom Vision API のプロジェクトを作成 ---
-
-
+# ペット情報のjsonをスプレッドシートから取得
 def get_pets_data():
-    # url = f"{os.environ.get('EndPoint')}customvision/v3.3/Training/projects?name=testcv002"
     url = "https://sheets.googleapis.com/v4/spreadsheets/1fIePGDbSQ80xQrK6QwWfb5EPQKtl1pcTgtfiQSs0knE/values/pets?key=AIzaSyCwIfAOPR1euxoN-7_UcbMIjRSWzwH3IIo"
     response = requests.get(url)
     return response
 
-# --- Costom Vision API のプロジェクトを作成 ---
-
-
+# 　健康診断情報のjsonをスプレッドシートから取得
 def get_health_checkup_data():
-    # url = f"{os.environ.get('EndPoint')}customvision/v3.3/Training/projects?name=testcv002"
     url = "https://sheets.googleapis.com/v4/spreadsheets/1fIePGDbSQ80xQrK6QwWfb5EPQKtl1pcTgtfiQSs0knE/values/健康診断?key=AIzaSyCwIfAOPR1euxoN-7_UcbMIjRSWzwH3IIo"
     response = requests.get(url)
     return response
-
 
 @app.route("/")
 def helloworld():
     return "<p>Hello world</p>"
 
-
-@app.route("/output")
+@app.route("/output",methods=["GET"])
 def goutput_data():
+    # パラメータの取得z
+    contents = request.args.get('q', '')
     # データ取得
     pets_r = get_pets_data().json()
     health_checkup_r = get_health_checkup_data().json()
@@ -36,28 +30,27 @@ def goutput_data():
     keys01 = pets_r["values"][0]
     values01 = pets_r["values"][1:]
     results01 = [dict(zip(keys01, item)) for item in values01]
-
     # データ成形(health_checkup)
     keys02 = health_checkup_r["values"][0]
     values02 = health_checkup_r["values"][1:]
     results02 = [dict(zip(keys02, item)) for item in values02]
-
     # PetIDを軸にしてDictを結合
     for i in results01:
       petid = i["PetID"]
+      # 辞書型から指定した値を持つものをデータを取得
       # ❓petIDはnumber型でいいのか❓今はいけてる
       include_id_list = list(filter(lambda item : item['PetID'] == petid, results02))
       i["helth"] = include_id_list[0:]
+    # もしパラメータが来たら指定された値を持つ情報を返す
+    if contents:
+        results01 = list(filter(lambda item : item['PetID'] == contents, results01))
 
-    # 辞書型から指定した値を持つものをデータを取得
-    # print(list(filter(lambda item : item['name'] == 'Taro', results)))
     return results01
 
-
-@app.route("/ou")
-def goutp_data():
-    health_checkup_r = get_health_checkup_data().json()
-    keys = health_checkup_r["values"][0]
-    values = health_checkup_r["values"][1:]
-    results = [dict(zip(keys, item)) for item in values]
-    return results
+# @app.route("/ou")
+# def goutp_data():
+#     health_checkup_r = get_health_checkup_data().json()
+#     keys = health_checkup_r["values"][0]
+#     values = health_checkup_r["values"][1:]
+#     results = [dict(zip(keys, item)) for item in values]
+#     return results
